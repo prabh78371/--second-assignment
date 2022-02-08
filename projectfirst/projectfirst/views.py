2f39fb35e4c282c9c0023cb2c95204eb44b2d050
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-02-08 16:06:34 +0530

    Add files via upload

---
+++ projectfirst/projectfirst/views.py
@@ -0,0 +1,8 @@
+# created by prabhjot kaur
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Good afternoon Prabhjot kaur")
+
+def pkaur(request):
+    return HttpResponse("How are you today?")
