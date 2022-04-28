7251d9704d9abd307f15eb2e8bc9eb48d3fbafcb
prabhjot kaur <99008608+prabh78371@users.noreply.github.com>
2022-04-28 13:34:27 +0530

    fix(views.py)
    add comment

--- projectfirst/projectfirst/views.py
+++ projectfirst/projectfirst/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# views
 def homepage(request):
     Name = {'name' : 'Prabhjot'}
     return render(request, 'priya.html', Name)
